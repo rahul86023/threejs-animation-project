752871e591c9c3de26bcc07811ec0467afba9959
Rahul Chaurasia <rahulchaurasia966@gmail.com>
2023-10-17 14:56:00 +0530

    movement added and build created

--- src/Components/Box.jsx
+++ src/Components/Box.jsx
@@ -1,14 +1,18 @@
-/* eslint-disable react/no-unknown-property */
-
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 
-
-export default function Box({texture}) {
+export default function Box({ texture }) {
   const colorMap = useLoader(TextureLoader, texture);
+
+  useFrame((state) => {
+    // Rotate the box automatically
+    state.scene.children[0].rotation.x += 0.005;
+    state.scene.children[0].rotation.y += 0.005;
+  });
+
   return (
-    <mesh rotation={[90, 0, 20]}>
-      <boxGeometry attach="geometry" args={[3, 3, 3]} /> 
+    <mesh>
+      <boxGeometry attach="geometry" args={[3, 3, 3]} />
       <meshStandardMaterial map={colorMap} />
     </mesh>
   );
--- src/Components/CanvasContainer.jsx
+++ src/Components/CanvasContainer.jsx
@@ -1,19 +1,12 @@
-/* eslint-disable react/no-unknown-property */
-
 import styled from "styled-components";
-
-
 import { Suspense } from "react";
-
-import { Canvas } from "@react-three/fiber";
+import { useFrame,Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-
 import Box from "./Box";
 
-export default function CanvasContainer({texture}) {
+export default function CanvasContainer({ texture }) {
   return (
     <Wrapper className="App">
- 
       <Canvas className="canvas">
         <OrbitControls enableZoom={false} />
         <ambientLight intensity={2} />
@@ -21,6 +14,7 @@ export default function CanvasContainer({texture}) {
         <Suspense fallback={null}>
           <Box texture={texture} />
         </Suspense>
+        <CustomAnimation /> {/* Add a custom animation component */}
       </Canvas>
     </Wrapper>
   );
@@ -28,8 +22,23 @@ export default function CanvasContainer({texture}) {
 
 const Wrapper = styled.div`
   position: relative;
-  background: #1f1144;
+  background: rgb(31, 17, 68);
   canvas {
     height: 500px;
   }
 `;
+
+// Custom animation component for automatically moving objects
+function CustomAnimation() {
+  // Use the useFrame hook for animation
+  useFrame(({ scene }) => {
+    // Access and update the rotation of the object
+    const box = scene.children.find((child) => child.type === "Mesh");
+    if (box) {
+      box.rotation.x += 0.005;
+      box.rotation.y += 0.005;
+    }
+  });
+
+  return null;
+}
